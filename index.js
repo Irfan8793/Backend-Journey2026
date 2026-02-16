b6dfa13e11135157c3c9f3485a4d03ef2bfbdb99
Irfan Khan <irfanpathan3524@gmail.com>
2026-02-16 22:00:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config() 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('Irfandotcom')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>Please login at twitterdotcom</h1>')
+})
+
+app.get('/irfan',(req,res)=>{
+    res.send("<h2>Irfan and Samreen</h2>")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
